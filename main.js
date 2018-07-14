2975d22df812eb36ead4949120649b7f3791c42a
Jonathan Lam <jlam55555@gmail.com>
2018-07-14 19:05:42 -0400

    fixed script before dom render

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ renderer.setSize( 640, 480 );
 renderer.domElement.style.position = 'absolute'
 renderer.domElement.style.top = '0px'
 renderer.domElement.style.left = '0px'
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
 // array of functions for the rendering loop
 var onRenderFcts= [];
