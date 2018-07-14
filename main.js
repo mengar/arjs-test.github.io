536631eb591ca529f051ca1a30efd7f017ad32c8
Jonathan Lam <jlam55555@gmail.com>
2018-07-14 19:03:07 -0400

    switched to using ar.js

--- main.js
+++ main.js
@@ -1,146 +1,151 @@
-let vrDisplay,
-    vrFrameData,
-    vrControls,
-    arView;
+//////////////////////////////////////////////////////////////////////////////////
+//    Init
+//////////////////////////////////////////////////////////////////////////////////
 
-let canvas,
-    camera,
-    scene,
-    renderer,
-    cube,
-    cubes = [];
-
-let anchorManager;
-
-let CUBE_SIZE_IN_METERS = .18;
-
-let colors = [
-  new THREE.Color(0xffffff),
-  new THREE.Color(0xffff00),
-  new THREE.Color(0xff00ff),
-  new THREE.Color(0xff0000),
-  new THREE.Color(0x00ffff),
-  new THREE.Color(0x00ff00),
-  new THREE.Color(0x0000ff),
-  new THREE.Color(0x000000)
-];
-
-THREE.ARUtils.getARDisplay().then(display => {
-  if(display) {
-    vrFrameData = new VRFrameData();
-    vrDisplay = display;
-    init();
-  } else {
-    THREE.ARUtils.displayUnsupportedMessage();
-  }
+// init renderer
+var renderer  = new THREE.WebGLRenderer({
+  antialias: true,
+  alpha: true
 });
+renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+renderer.setSize( 640, 480 );
+renderer.domElement.style.position = 'absolute'
+renderer.domElement.style.top = '0px'
+renderer.domElement.style.left = '0px'
+document.body.appendChild( renderer.domElement );
 
-let init = () => {
-  // turn on debugging
-  let arDebug = new THREE.ARDebug(vrDisplay);
-  document.body.append(arDebug.getElement());
+// array of functions for the rendering loop
+var onRenderFcts= [];
 
-  // set up the three.js rendering environment
-  renderer = new THREE.WebGLRenderer({ alpha: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.autoClear = false;
+// init scene and camera
+var scene = new THREE.Scene();
   
-  canvas = renderer.domElement;
-  document.body.appendChild(canvas);
-  scene = new THREE.Scene();
+//////////////////////////////////////////////////////////////////////////////////
+//    Initialize a basic camera
+//////////////////////////////////////////////////////////////////////////////////
 
-  // create ARView, which handles renderering of camera scene behind three.js context
-  arView = new THREE.ARView(vrDisplay, renderer);
+// Create a camera
+var camera = new THREE.Camera();
+scene.add(camera);
 
-  // ARPerspectiveCamera a little different THREE
-  camera = new THREE.ARPerspectiveCamera(
-    vrDisplay, 60, window.innerWidth / window.innerHeight,
-    vrDisplay.depthNear, vrDisplay.depthFar);
+////////////////////////////////////////////////////////////////////////////////
+//          handle arToolkitSource
+////////////////////////////////////////////////////////////////////////////////
 
-  // keep real and virtual world in sync
-  vrControls = new THREE.VRControls(camera);
-
-  // cube geometry to copy when user clicks on the screen
-  let geometry = new THREE.BoxGeometry(0.05, 0.05, 0.05);
-  let faceIndeces = ['a', 'b', 'c'];
-  geometry.faces.forEach(face => {
-    faceIndeces.forEach((faceIndex, index) => {
-      let vertexIndex = face[faceIndex];
-      face.vertexColors[index] = colors[vertexIndex];
-    })
-  });
-  let material = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
-  cube = new THREE.Mesh(geometry, material);
-
-  // bind event listeners
-  window.addEventListener('resize', onWindowResize, false);
-  window.addEventListener('touchstart', onClick, false);
-
-  anchorManager = new THREE.ARAnchorManager(vrDisplay);
+var arToolkitSource = new THREEx.ArToolkitSource({
+  // to read from the webcam 
+  sourceType : 'webcam',
   
-  // begin render loop
-  update();
-};
+  // // to read from an image
+  // sourceType : 'image',
+  // sourceUrl : THREEx.ArToolkitContext.baseURL + '../data/images/img.jpg',    
 
-// render loop
-let update = () => {
-  renderer.clearColor();
-  arView.render();
-  camera.updateProjectionMatrix();
-  vrDisplay.getFrameData(vrFrameData);
-  vrControls.update();
-  renderer.clearDepth();
-  renderer.render(scene, camera);
-  requestAnimationFrame(update);
-};
+  // to read from a video
+  // sourceType : 'video',
+  // sourceUrl : THREEx.ArToolkitContext.baseURL + '../data/videos/headtracking.mp4',   
+})
 
-// on window resize
-let onWindowResize = e => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-};
+arToolkitSource.init(function onReady(){
+  onResize()
+})
 
-// when clicking, create cube at user's current position
-let onClick = e => {
-  // if touched with two+ fingers, remove latest models and and its anchor
-  if(cubes.length > 0 && e.touches.length > 1) {
-    anchorManager.remove(cubes[0]);
-    scene.remove(cubes[0]);
-    cubes.splice(0, 1);
-    return;
-  }
+// handle resize
+window.addEventListener('resize', function(){
+  onResize()
+})
+function onResize(){
+  arToolkitSource.onResize()  
+  arToolkitSource.copySizeTo(renderer.domElement) 
+  if( arToolkitContext.arController !== null ){
+    arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)  
+  } 
+}
+////////////////////////////////////////////////////////////////////////////////
+//          initialize arToolkitContext
+////////////////////////////////////////////////////////////////////////////////
 
-  // fetch pose data from current frame
-  let pose = vrFrameData.pose;
 
-  // convert pose orientation and position into objects
-  let ori = new THREE.Quaternion(
-    pose.orientation[0],
-    pose.orientation[1],
-    pose.orientation[2],
-    pose.orientation[3]
-  );
-  let pos = new THREE.Vector3(
-    pose.position[0],
-    pose.position[1],
-    pose.position[2]
-  );
-  let dirMtx = new THREE.Matrix4();
-  dirMtx.makeRotationFromQuaternion(ori);
-  let push = new THREE.Vector3(0, 0, -1.0);
-  push.transformDirection(dirMtx);
-  pos.addScaledVector(push, 0.125);
+// create atToolkitContext
+var arToolkitContext = new THREEx.ArToolkitContext({
+  cameraParametersUrl: THREEx.ArToolkitContext.baseURL + '../data/data/camera_para.dat',
+  detectionMode: 'mono',
+})
+// initialize it
+arToolkitContext.init(function onCompleted(){
+  // copy projection matrix to camera
+  camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
+})
 
-  // clone cube object and place at location
-  var cubeClone = cube.clone();
-  scene.add(cubeClone);
-  cubeClone.position.copy(pos);
-  cubeClone.quaternion.copy(ori);
+// update artoolkit on every frame
+onRenderFcts.push(function(){
+  if( arToolkitSource.ready === false ) return
 
-  cubes.push(cubeClone);
-  anchorManager.add(cubeClone);
+  arToolkitContext.update( arToolkitSource.domElement )
   
-};
+  // update scene.visible if the marker is seen
+  scene.visible = camera.visible
+})
+  
+////////////////////////////////////////////////////////////////////////////////
+//          Create a ArMarkerControls
+////////////////////////////////////////////////////////////////////////////////
+
+// init controls for camera
+var markerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
+  type : 'pattern',
+  patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.hiro',
+  // patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.kanji',
+  // as we controls the camera, set changeMatrixMode: 'cameraTransformMatrix'
+  changeMatrixMode: 'cameraTransformMatrix'
+})
+// as we do changeMatrixMode: 'cameraTransformMatrix', start with invisible scene
+scene.visible = false
+
+//////////////////////////////////////////////////////////////////////////////////
+//    add an object in the scene
+//////////////////////////////////////////////////////////////////////////////////
+
+// add a torus knot 
+var geometry  = new THREE.CubeGeometry(1,1,1);
+var material  = new THREE.MeshNormalMaterial({
+  transparent : true,
+  opacity: 0.5,
+  side: THREE.DoubleSide
+}); 
+var mesh  = new THREE.Mesh( geometry, material );
+mesh.position.y = geometry.parameters.height/2
+scene.add( mesh );
+
+var geometry  = new THREE.TorusKnotGeometry(0.3,0.1,64,16);
+var material  = new THREE.MeshNormalMaterial(); 
+var mesh  = new THREE.Mesh( geometry, material );
+mesh.position.y = 0.5
+scene.add( mesh );
+
+onRenderFcts.push(function(delta){
+  mesh.rotation.x += Math.PI*delta
+})
+
+//////////////////////////////////////////////////////////////////////////////////
+//    render the whole thing on the page
+//////////////////////////////////////////////////////////////////////////////////
+
+// render the scene
+onRenderFcts.push(function(){
+  renderer.render( scene, camera );
+})
+
+// run the rendering loop
+var lastTimeMsec= null
+requestAnimationFrame(function animate(nowMsec){
+  // keep looping
+  requestAnimationFrame( animate );
+  // measure time
+  lastTimeMsec  = lastTimeMsec || nowMsec-1000/60
+  var deltaMsec = Math.min(200, nowMsec - lastTimeMsec)
+  lastTimeMsec  = nowMsec
+  // call each update function
+  onRenderFcts.forEach(function(onRenderFct){
+    onRenderFct(deltaMsec/1000, nowMsec/1000)
+  })
+})
