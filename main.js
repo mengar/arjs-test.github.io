c3b947ffd45d1362afb947b9eea86fe4db0ff466
Jonathan Lam <jlam55555@gmail.com>
2018-07-14 19:40:13 -0400

    added camera parameters file

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ function onResize(){
 
 // create atToolkitContext
 var arToolkitContext = new THREEx.ArToolkitContext({
-  cameraParametersUrl: './patt.hiro',
+  cameraParametersUrl: '../data/camera_para.dat',
   detectionMode: 'mono',
 })
 // initialize it
@@ -93,7 +93,7 @@ onRenderFcts.push(function(){
 // init controls for camera
 var markerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
   type : 'pattern',
-  patternUrl : '../data/data/patt.hiro',
+  patternUrl : '../data/patt.hiro',
   // patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.kanji',
   // as we controls the camera, set changeMatrixMode: 'cameraTransformMatrix'
   changeMatrixMode: 'cameraTransformMatrix'
